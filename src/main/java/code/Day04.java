ccce254c6c28fcad5b56d5ae19dfb122d16459e6
Robert Seitz <robert.seitz@stroehmersoft.de>
2020-12-04 19:00:26 +0100

    Day Four - initial commit

---
+++ src/main/java/code/Day04.java
@@ -0,0 +1,28 @@
+package code;
+
+import shared.Reader;
+
+public class Day04 {
+    private static final Reader reader = new Reader();
+    private static final String path = "src/main/resources/day03.txt";
+
+    public static void main(String[] args) {
+        Day04 logic = new Day04();
+        System.out.println("\n------ DAY THREE ------\n");
+        logic.partOne();
+        logic.partTwo();
+    }
+
+    private void setUp(){
+        
+    }
+
+    private void partOne(){
+
+    }
+
+    private void partTwo(){
+
+    }
+
+}
